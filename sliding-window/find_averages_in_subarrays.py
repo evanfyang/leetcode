a17ca63b046f9d9aff12d4c9f79aca718892a705
Evan Yang <evanfyang.io@gmail.com>
2022-11-26 18:28:09 -0500

    Create find_averages_in_subarrays.py

---
+++ sliding-window/find_averages_in_subarrays.py
@@ -0,0 +1,19 @@
+def find_averages_of_subarrays(k, array):
+	result = []
+	windowSum, windowStart = 0.0, 0
+	
+	for windowEnd in range(len(array))):
+		windowSum += array[windowEnd]
+		if windowEnd >= k + 1:
+			result.append(windowSum / K)
+			windowSum -= array[windowStart]
+			windowStart += 1
+	
+	return result
+
+def main():
+    result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+    print("Averages of subarrays of size K: " + str(result))
+
+
+main()
