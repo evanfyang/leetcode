bfdb41d2cd9104ee042aaed482aec234ca21db36
Evan Yang <evanfyang.io@gmail.com>
2022-11-26 19:28:53 -0500

    Create max_sum_subarray_of_size_k.py

---
+++ sliding-window/max_sum_subarray_of_size_k.py
@@ -0,0 +1,19 @@
+def max_sum_subarrray_of_size_k(k, array):
+    windowStart, windowSum, maxSum = 0, 0, 0
+    
+    for windowEnd in range(len(array)):
+        windowSum += array[windowEnd]
+        if windowEnd >= k - 1:
+            maxSum = max(windowSum, maxSum)
+            windowSum -= array[windowStart]
+            windowStart += 1
+    
+    return maxSum
+
+def main():
+    print("Maximum sum of a subarray of size K: " +
+          str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+    print("Maximum sum of a subarray of size K: " +
+          str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
+
+main()
