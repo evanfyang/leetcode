bc42572f5143d06d2454b281b2bd0e5d2940b225
Evan Yang <evanfyang.io@gmail.com>
2022-12-12 21:51:43 -0500

    implement minimum size subarray with greater sum

---
+++ sliding-window/209_minimum_size_subarray_with_greater_sum.py
@@ -0,0 +1,15 @@
+def minimum_size_subarray_with_greater_sum(target, nums):
+    windowStart, windowSum, minLength = 0, 0, 0
+    
+    for windowEnd in range(length(nums)):
+        windowSum += nums[windowEnd]
+        while windowSum > target:
+            minLength = min(minLength, windowEnd + 1 - windowStart)
+            windowSum -= nums[windowStart]
+            windowStart += 1
+    
+    return minLength
+
+target = 7, nums = [2,3,1,2,4,3]
+print(minimum_size_subarray_with_greater_sum(target, nums))
+            
