515875e90a9ab6fdc41fd06092a467cff686b547
Evan Yang <evanfyang.io@gmail.com>
2022-12-19 16:36:22 -0500

    Create 567_permutations_in_string.py

---
+++ sliding-window/567_permutations_in_string.py
@@ -0,0 +1,39 @@
+def permutations_in_string(string, pattern):
+    windowStart, matched_chars = 0, 0
+    char_frequency = dict()
+
+    for char in pattern:
+        if char not in char_frequency:
+            char_frequency[char] = 1
+        else:
+            char_frequency[char] += 1
+
+    for windowEnd in range(len(string)):
+        if string[windowEnd] in char_frequency:
+            char_frequency[string[windowEnd]] -= 1
+            if char_frequency[string[windowEnd]] == 0:
+                matched_chars += 1
+
+        if matched_chars == len(char_frequency):
+            return True
+
+        if (windowEnd + 1 >= len(s1)):
+            if string[windowStart] in char_frequency:
+                if char_frequency[string[windowStart]] == 0:
+                    matched_chars -= 1
+                char_frequency[string[windowStart]] += 1
+            windowStart += 1
+
+    return False
+
+def main():
+    string = "eidbaooo", pattern = "ab"
+    print("Input: " + "string = " + string + ", pattern = " + pattern)    
+    print("Output: " + permutations_in_string(string, pattern))
+    
+    string = "eidboaoo", pattern = "ab"
+    print("Input: " + "string = " + string + ", pattern = " + pattern)    
+    print("Output: " + permutations_in_string(string, pattern))
+
+main()
+    
