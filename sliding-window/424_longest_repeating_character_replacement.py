abac9a87b17a3da8a961a2ac7531b8102aa4797c
Evan Yang <evanfyang.io@gmail.com>
2022-12-19 11:45:58 -0500

    Create 424_longest_repeating_character_replacement.py

---
+++ sliding-window/424_longest_repeating_character_replacement.py
@@ -0,0 +1,30 @@
+def longest_repeating_character_replacement(k, string):
+    windowStart, maxRepeatingCharacterCount, maxLength = 0, 0, 0
+    characterFrequency = dict()
+    
+    for windowEnd in range(len(string)):
+        if string[windowEnd] not in characterFrequency:
+            characterFrequency[string[windowEnd]] = 1
+        else:
+            characterFrequency[string[windowEnd]] += 1
+            
+        maxRepeatingCharacterCount = max(maxRepeatingCharacterCount, characterFrequency[string[windowEnd]])
+        
+        if ((windowEnd - windowStart + 1) - maxRepeatingCharacterCount > k):
+            characterFrequency[string[windowStart]] -= 1
+            windowStart += 1
+            
+        maxLength = max(maxLength, windowEnd - windowStart + 1)
+    
+    return maxLength
+
+def main():
+    k = 2, string = "ABAB"
+    print("Input: " + "k = " + k + ", string = " + string)    
+    print("Output: " + longest_repeating_character_replacement(k, string))
+    
+    k = 1, string = "AABABBA"
+    print("Input: " + "k = " + k + ", string = " + string)    
+    print("Output: " + longest_repeating_character_replacement(k, string))
+
+main()
