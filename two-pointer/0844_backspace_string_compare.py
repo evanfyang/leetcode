c0923cb532386a47651e138ad285af9ac2105416
Evan Yang <evanfyang.io@gmail.com>
2022-12-25 23:35:30 -0500

    solved 0844 - backspace string compare

---
+++ two-pointer/0844_backspace_string_compare.py
@@ -0,0 +1,71 @@
+def backspace_string_compare(string_1, string_2):
+    string_1_index = len(string_1) - 1
+    string_2_index = len(string_2) - 1
+
+    while string_1_index >= 0 or string_2_index >= 0:
+        next_string_1_index = self.get_next_valid_character_index(string_1, string_1_index)
+        next_string_2_index = self.get_next_valid_character_index(string_2, string_2_index)
+
+        if next_string_1_index < 0 and next_string_2_index < 0:
+            return True
+        if next_string_1_index < 0 or next_string_2_index < 0:
+            return False
+        if string_1[next_string_1_index] != string_2[next_string_2_index]:
+            return False
+
+        string_1_index = next_string_1_index - 1
+        string_2_index = next_string_2_index - 1
+
+    return True
+
+def get_next_valid_character_index(self, string, index):
+    backspace_count = 0
+
+    while index >= 0:
+        if string[index] == "#":
+            backspace_count += 1
+        elif backspace_count > 0:
+            backspace_count -= 1
+        else:
+            break
+        index -= 1
+
+    return index
+
+def main():
+    string_1 = "xy#z"
+    string_2 = "xzz#"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+    
+    string_1 = "xy#z"
+    string_2 = "xyz#"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+    
+    string_1 = "xp#"
+    string_2 = "xyz##"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+
+    string_1 = "xywrrmp"
+    string_2 = "xywrrmu#p"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+
+    string_1 = "ab#c"
+    string_2 = "ad#c"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+    
+    string_1 = "ab##"
+    string_2 = "c#d#"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+    
+    string_1 = "a#c"
+    string_2 = "b"
+    print("Input: string_1 = " + string_1 + ", string_2 = " + string_2)
+    print("Output: " + str(backspace_string_compare(string_1, string_2)))
+    
+main()
