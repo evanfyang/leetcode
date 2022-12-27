a077079ddfad8fe229b53fc80597e07638fc960b
Evan Yang <evanfyang.io@gmail.com>
2022-12-26 22:12:11 -0500

    solved 234 - palindrome linked list

---
+++ fast-and-slow-pointers/234_palindrome_linked_list.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+class node:
+    def __init__(self, value = 0, next = None):
+        self.value = value
+        self.next = next
+
+def palindrome_linked_list(head):
+    slow_pointer = head
+    fast_pointer = head
+
+    while fast_pointer is not None and fast_pointer.next is not None:
+        fast_pointer = fast_pointer.next.next 
+        slow_pointer = slow_pointer.next
+
+    head_second_half_reversed = reverse(slow_pointer)
+    copy_head_second_half_reversed = head_second_half_reversed
+    head_first_half = head
+    
+    while head_first_half is not None and head_second_half_reversed is not None:
+        if head_first_half.value != head_second_half_reversed.value:
+            return False
+        head_first_half = head_first_half.next
+        head_second_half_reversed = head_second_half_reversed.next
+
+    self.reverse(copy_head_second_half_reversed)
+    
+    if head_first_half is None or head_second_half_reversed is None:
+        return True
+
+def reverse(head):
+    previous_node = None
+
+    while head is not None:
+        next_node = head.next
+        head.next = previous_node
+        previous_node = head
+        head = next_node
+
+    return previous_node
+
+def main():
+    head = node(2)
+    head.next = node(4)
+    head.next.next = node(6)
+    head.next.next.next = node(4)
+    head.next.next.next.next = node(2)
+    print("Is palindrome: " + str(palindrome_linked_list(head)))
+
+    head.next.next.next.next.next = node(2)
+    print("Is palindrome: " + str(palindrome_linked_list(head)))
+    
+    head = node(1)
+    head.next = node(2)
+    head.next.next = node(2)
+    head.next.next.next = node(1)
+    print("Is palindrome: " + str(palindrome_linked_list(head)))
+
+    head = node(1)
+    head.next = node(2)
+    print("Is palindrome: " + str(palindrome_linked_list(head)))
+    
+main()
