d3a822bafb834728c8dc7a7a9885c445c66bbffa
Evan Yang <evanfyang.io@gmail.com>
2023-01-03 13:08:28 -0500

    solved 143 - reorder list

---
+++ fast-and-slow-pointers/143_reorder_list.py
@@ -0,0 +1,61 @@
+# Definition for singly-linked list.
+class node:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+    
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(str(temp.value) + " ", end='')
+            temp = temp.next
+        print()
+
+def reorder_list(head):
+    if head is None and head.next is None: 
+        return 
+
+    slow_pointer = head
+    fast_pointer = head
+
+    while fast_pointer is not None and fast_pointer.next is not None:
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
+
+    head_second_half = self.reverse(slow_pointer)
+    head_first_half = head
+
+    while head_first_half is not None and head_second_half is not None:
+        first_half_next_node = head_first_half.next
+        head_first_half.next = head_second_half 
+        head_first_half = first_half_next_node
+
+        second_half_next_node = head_second_half.next
+        head_second_half.next = head_first_half
+        head_second_half = second_half_next_node
+
+    if head_first_half is not None:
+        head_first_half.next = None
+
+def reverse(self, head):
+    previous_node = None
+    while head is not None:
+        next_node = head.next
+        head.next = previous_node
+        previous_node = head
+        head = next_node
+    return previous_node
+
+def main():
+    
+    print("Input: " + head.print_list())
+    head = node(2)
+    head.next = node(4)
+    head.next.next = node(6)
+    head.next.next.next = node(8)
+    head.next.next.next.next = node(10)
+    head.next.next.next.next.next = node(12)
+    reorder(head)
+    print("Output: " + head.print_list())
+
+main()
