604af6a30cc84b1d753b84423805125125c52a3d
Evan Yang <evanfyang.io@gmail.com>
2023-01-04 21:57:03 -0500

    Update cyclic_sort.py

--- cyclic-sort/cyclic_sort.py
+++ cyclic-sort/cyclic_sort.py
@@ -7,8 +7,16 @@ def cyclic_sort(nums):
     return nums
 
 def main():
-  print(str(cyclic_sort([3, 1, 5, 4, 2])))
-  print(str(cyclic_sort([2, 6, 4, 3, 1, 5])))
-  print(str(cyclic_sort([1, 5, 6, 4, 3, 2])))
+    nums = [3,1,5,4,2]
+    print("Input: nums = " + str(nums))
+    print("Output: " + str(cyclic_sort(nums)))
+    
+    nums = [2,6,4,3,1,5]
+    print("Input: nums = " + str(nums))
+    print("Output: " + str(cyclic_sort(nums)))
+    
+    nums = [1,5,6,4,3,2]
+    print("Input: nums = " + str(nums))
+    print("Output: " + str(cyclic_sort(nums)))
 
 main()
