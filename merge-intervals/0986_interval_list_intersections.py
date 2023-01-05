5fdbfc2690917209210cd7bb6f29ff874c7f86f0
Evan Yang <evanfyang.io@gmail.com>
2023-01-04 19:24:15 -0500

    added additional solutions to 986 - interval list intersections

--- merge-intervals/0986_interval_list_intersections.py
+++ merge-intervals/0986_interval_list_intersections.py
@@ -1,4 +1,4 @@
-def interval_list_intersections(first_list, second_list):
+def interval_list_intersections_grokking(first_list, second_list):
     intersections = list()
     i, j, start, end = 0, 0, 0, 1
 
@@ -6,6 +6,43 @@ def interval_list_intersections(first_list, second_list):
         first_overlaps_second = first_list[i][start] >= second_list[j][start] and first_list[i][start] <= second_list[j][end]
 
         second_overlaps_first = second_list[j][start] >= first_list[i][start] and second_list[j][start] <= first_list[i][end]
+
+        if first_overlaps_second or second_overlaps_first:
+            intersections.append([max(first_list[i][start], second_list[j][start]), min(first_list[i][end], second_list[j][end])])
+
+        if first_list[i][end] < second_list[j][end]: 
+            i += 1
+        else:
+            j += 1
+
+    return intersections
+
+def interval_list_intersection_leetcode(first_list, second_list):
+    intersections = list()
+    i, j, start, end = 0, 0, 0, 1
+
+    while i < len(first_list) and j < len(second_list):
+        low = max(first_list[i][start], second_list[j][start])
+        high = min(first_list[i][end], second_list[j][end])
+
+        if low <= high:
+            intersections.append([low, high])
+
+        if first_list[i][end] < second_list[j][end]: 
+            i += 1
+        else:
+            j += 1
+
+    return intersections
+
+def interval_list_intersections(first_list, second_list):
+    intersections = list()
+    i, j, start, end = 0, 0, 0, 1
+
+    while i < len(first_list) and j < len(second_list):
+        first_overlaps_second = first_list[i][start] <= second_list[j][end] and first_list[i][end] >= second_list[j][start]
+
+        second_overlaps_first = second_list[j][start] <= first_list[i][end] and second_list[j][end] >= first_list[i][start]
 
         if first_overlaps_second or second_overlaps_first:
             intersections.append([max(first_list[i][start], second_list[j][start]), min(first_list[i][end], second_list[j][end])])
