8b713791d8ecce8cd13d57345d58bfc993b6ce0b
evanfyang <evanfyang.io@gmail.com>
2023-02-03 19:18:03 -0500

    solved 125 - valid palindrome

---
+++ two-pointer/0125_valid_palindrome.py
@@ -0,0 +1,40 @@
+def valid_palindrome(string):
+    left, right = 0, len(string) - 1
+    string = string.lower()
+
+    while left < right:
+        while left < right and not string[left].isalnum():
+            left += 1
+        while left < right and not string[right].isalnum():
+            right -= 1
+        
+        if string[left] != string[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+def main():
+    string = "A man, a plan, a canal: Panama"
+    print("Input: sting = " + str(string))
+    print("Output: " + str(valid_palindrome(string))) 
+    print() 
+
+    string = "race a car"
+    print("Input: sting = " + str(string))
+    print("Output: " + str(valid_palindrome(string))) 
+    print() 
+
+    string = " "
+    print("Input: sting = " + str(string))
+    print("Output: " + str(valid_palindrome(string))) 
+    print() 
+
+    string = "Was it a car or a cat I saw?"
+    print("Input: sting = " + str(string))
+    print("Output: " + str(valid_palindrome(string))) 
+    print() 
+
+main()
