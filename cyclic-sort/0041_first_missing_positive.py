245639229e09c85cb4edbb385f5ca8ec0d2a357d
evanfyang <evanfyang.io@gmail.com>
2023-02-06 13:53:55 -0500

    fixed typo

--- cyclic-sort/0041_first_missing_positive.py
+++ cyclic-sort/0041_first_missing_positive.py
@@ -16,34 +16,42 @@ def first_missing_positive(nums):
 def main():
     nums = [-3,1,5,4,2]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
+    print("Output: " + str(first_missing_positive(nums)))
+    print()
     
     nums = [3,-2,0,1,2]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
+    print("Output: " + str(first_missing_positive(nums)))
+    print()
     
     nums = [3,2,5,1]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
-    
+    print("Output: " + str(first_missing_positive(nums)))
+    print()
+
     nums = [33,37,5]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
-    
-    nums = [1,2,0]
-    print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
+    print("Output: " + str(first_missing_positive(nums)))
+    print()    
 
     nums = [1,2,0]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
-    
+    print("Output: " + str(first_missing_positive(nums)))
+    print()
+
+    nums = [1,2,0]
+    print("Input: nums = " + str(nums))
+    print("Output: " + str(first_missing_positive(nums)))
+    print()    
+
     nums = [3,4,-1,1]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
-    
+    print("Output: " + str(first_missing_positive(nums)))
+    print()
+
     nums = [7,8,9,11,12]
     print("Input: nums = " + str(nums))
-    print("Output: " + str(find_duplicate_number(nums)))
-    
+    print("Output: " + str(first_missing_positive(nums)))
+    print()
+
 main()
