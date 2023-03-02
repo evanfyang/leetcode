d97a4753d7a43ee71402b50f2082306cce66bc81
evanfyang <evanfyang.io@gmail.com>
2023-03-02 14:47:59 -0500

    added comments

--- binary-trees/0110_balanced_binary_tree.py
+++ binary-trees/0110_balanced_binary_tree.py
@@ -21,12 +21,22 @@ def generate_binary_tree_from_array(array, array_length, array_index):
 
 def balanced_binary_tree(root):
     def dfs(root):
+        # base case: when current root of tree is None, return True
+        # for balanced and height of 0
         if root is None: 
             return [True, 0]
 
+        # check if right and left subtree are balanced and get their heights
         left_subtree, right_subtree = dfs(root.left), dfs(root.right)
+        # check if the tree at the current root is balanced by checking if 
+        # both left and right subtree are balanced and if the difference in 
+        # height between the left and right subtree is equal to or less than 1
         balanced = left_subtree[0] and right_subtree[0] and abs(left_subtree[1] - right_subtree[1]) <= 1
 
+        # return if the tree at the current root is balanced and the height
+        # of the tree at the current root; height of the tree at the current 
+        # root is calculated by taking the max height of the two subtrees and 
+        # adding one to account for the root
         return [balanced, 1 + max(left_subtree[1], right_subtree[1])]
     
     return dfs(root)[0]
